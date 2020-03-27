fdfa8ee7eac8365d80ae18c6c44906b0bf6211fb
Blas Kolic <blas.kolic@ouce.ox.ac.uk>
2020-03-27 20:43:03 +0000

    Updated script. It now runs the model for Mexico with a given set of parameters.

--- run_SEIHRD_model.py
+++ run_SEIHRD_model.py
@@ -9,38 +9,20 @@ import datetime
 from scipy import integrate
 import seaborn as sns; sns.set()
 
-###---------###
-### Reading ###
-###---------###
-# be sure to git pull upstream master before reading the data so it is up to date.
-DATA_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/'
-DATA_URL_MEX = 'https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/'
-
 ###------------------###
 ### Helper Functions ###
 ###------------------###
 
-## For global
-def country_to_timeseries(df, country):
-    df_country = df[ df['Country/Region'] == country ].drop(['Province/State','Lat','Long'], axis=1)
-
-    return df_country.groupby('Country/Region').sum().T[country]
-
-def province_to_timeseries(df, province):
-    df_province = df[ df['Province/State'] == province ]
-
-    return df_province.set_index('Province/State').drop(['Country/Region','Lat','Long'], axis=1).T[province]
-
-## For Mexico
+## Time series management
 def statal_timeseries(df, log=False):
     '''
     Returns a dataframe with the number of COVID cases where each row is indexed by a date (t0 = 2020-02-28), and each column is a state of Mexico.
     If log=True, return the log of the cases.
     '''
     if log:
-        return np.log10( df.drop(['Mexico_pais'], axis=1).set_index('Fecha') )
+        return np.log10( df.drop(['México'], axis=1).set_index('Fecha') )
     else:
-        return df.drop(['Mexico_pais'], axis=1).set_index('Fecha')
+        return df.drop(['México'], axis=1).set_index('Fecha')
 
 def national_timeseries(df, log=False):
     '''
@@ -48,20 +30,38 @@ def national_timeseries(df, log=False):
     If log=True, return the log of the cases.
     '''
     if log:
-        return np.log10( df.set_index('Fecha').loc[:,['Mexico_pais']] )
+        return np.log10( df.set_index('Fecha').loc[:,['México']] )
     else:
-        return df.set_index('Fecha').loc[:,['Mexico_pais']]
+        return df.set_index('Fecha').loc[:,['México']]
 
-## Other helper Functions
+### PLOTTING HELPERS
+def plot_cases(projection, dataset, state, initial_date, ls='-', label_proj='projection', label_data=''):
 
-# helper to sum I+H+R+D cases
-def total_cases_timeseries(sol, N):
-    return (sol[:, 2] + sol[:, 3] + sol[:, 4] + sol[:, 5])*N
+    # the index of the data df is the date.
+    t0 = datetime.datetime.strptime( initial_date, '%Y-%m-%d')
+    # construct time array
+    t_range = [(t0 + datetime.timedelta(days=x)).strftime('%d-%m') for x in range( projection.shape[0] )]
+
+    if state != 'México':
+        data       = statal_timeseries(dataset, log=False).loc[initial_date:,state]
+    else:
+        data       = national_timeseries(dataset, log=False).loc[initial_date:,state]
+
+    plt.plot(t_range, projection, label=label_proj, ls=ls, c='grey')
+    plt.plot(data.values, lw=3, label=label_data, c='red')
+
+    plt.xticks(rotation=45)
+    plt.legend()
+
+    mae = MAE(data, projection)
+    print('MAE: {}'.format(mae))
+    return mae
 
 # mean absolute error
-def MAE(x,y): return np.mean(np.abs( x - y ))
+def MAE(x,y): return np.mean(np.abs( x - y[:len(x)] ))
 
-def sol_to_csv(filename, sol, initial_date, total_population):
+# takes a set of solutions, aggregates them and saves them in a csv
+def sol_to_csv(filename, sol, initial_date):
 
     try:
         # our format
@@ -69,36 +69,64 @@ def sol_to_csv(filename, sol, initial_date, total_population):
     except:
         # John Hopkins format
         t0 = datetime.datetime.strptime(initial_date, '%m/%d/%y')
-    t_range = [t0 + datetime.timedelta(days=x) for x in range( sol.shape[0] )]
-    CSV = pd.DataFrame(columns=['Totales','Recuperados','Muertes','Hospitalizados'])
 
-    CSV['Totales'] = total_cases_timeseries(sol, N=total_population)
-    CSV['Recuperados'] = sol[:,4] * total_population
-    CSV['Muertes'] = sol[:,5] * total_population
-    CSV['Hospitalizados'] = sol[:,3] * total_population
-    CSV.index = t_range
+    # this is thought of as a list of arrays
+#     (t0 + datetime.timedelta(days=x)).strftime('%d-%m')
+    t_range = [t0 + datetime.timedelta(days=x) for x in range( sol[0].shape[0] )]
+    CSV = pd.DataFrame(columns=['Fecha','Totales','Recuperados','Muertes','Hospitalizados'])
 
-    CSV.to_csv('data/'+filename)
+    CSV['Totales'] = CasesAggregation(sol, f=TotalCases)
+    CSV['Recuperados'] = CasesAggregation(sol, f=Recovered)
+    CSV['Muertes'] = CasesAggregation(sol, f=Deceased)
+    CSV['Hospitalizados'] = CasesAggregation(sol, f=Hospitalized)
+    CSV['Fecha'] = t_range
+    CSV.set_index('Fecha', inplace=True)
+
+    print('Saved projections in {}'.format(filename))
+    CSV.to_csv(filename)
+    return CSV
 
 ###------------------###
 ### Model Definition ###
 ###------------------###
 
+## Model helper functions
+# number of contacts
+def k(t, k_avg, κ0, σ, tc): return (1 - κ0*Θ(t,tc))*k_avg + κ0*(σ - 1)*Θ(t,tc)
+# probability of not getting infected
+def P(I,A, t, β, k_avg, κ0, σ, tc): return 1 - (1-β)**( k(t, k_avg, κ0, σ, tc)*(I+A) )
+# heaviside function
+def Θ(t,tc): return np.heaviside( t-tc, 1 )
+# kronecker delta
+def δ(t,tc):
+    if t == tc:
+        return 1
+    else:
+        return 0
+
 ### Non-compartamental ###
     ## Discrete time Markovian model ##
-def SEIHRD_markov_step(x, t, *params):
+def SEIHRD_markov_step(x, t, S_tc, CH_tc, *params):
     '''
     Suceptible (S), Exposed (E), Infected (I), Hospitalized (H), Recovered (R), Deceased (D) epidemic model.
     The function takes a single time step in the units of days.
+
+    When confinement is present, the model is no longer Markovian as the variables depend on the state of S_tc = S(tc) and CH_tc = CH(tc).
+    If tc = np.inf, then no confinement is made.
     '''
-    λ, κ, γI, μI, α, γH, μH = params
-    S,E,I,H,R,D = x
-    return [S*(1-λ*I),
-            λ*I*S + (1-κ)*E,
-            κ*E + (1 - (γI+μI+α))*I,
-            α*I + (1 - (γH+μH))*H,
-            γI*I + γH*H + R,
-            μI*I + μH*H + D]
+
+    β, k_avg, η, α, γI, μI, ν, γH, μH, *containtment_params = params
+    κ0, σ, tc = containtment_params
+    S,E,CH,A,I,H,R,D = x
+
+    return [S*(1 - P(I,A, t, β, k_avg, κ0, σ, tc)) * (1 - δ(t,tc)*κ0*CH_tc),     # S(t+1)
+            S*P(I,A, t, β, k_avg, κ0, σ, tc) * (1 - δ(t,tc)*κ0*CH_tc) + (1-η)*E, # E(t+1)
+            S_tc * κ0 * CH_tc * Θ(t,tc),                                         # CH(t+1)
+            η*E + (1-α)*A,                                                       # A(t+1)
+            α*A + (1 - (γI+μI+ν))*I,                                             # I(t+1)
+            ν*I + (1 - (γH+μH))*H,                                               # H(t+1)
+            γI*I + γH*H + R,                                                     # R(t+1)
+            μI*I + μH*H + D]                                                     # D(t+1)
 
 ### Solver ###
 def solve(f, x0, t0, n_steps, *params):
@@ -107,73 +135,182 @@ def solve(f, x0, t0, n_steps, *params):
     '''
     xt = [xi for xi in x0]
     sol = np.zeros( (n_steps, len(x0)) )
+    S_tc, CH_tc = 0, 0
+
+    (β, k_avg, η, α, γI, μI, ν, γH, μH, κ0, σ, tc) = params
 
     t = t0
     for (i,t) in enumerate( range(n_steps) ):
 
+        if t == tc:
+            S,E,CH,A,I,H,R,D = xt
+            S_tc, CH_tc = S, (S + R)**σ
+
         sol[i,:] = xt
 
-        xt = f(xt, 0.0, *params)
+        xt = f(xt, t, S_tc, CH_tc, *params)
         t += 1
 
     return sol
 
-if __name__ == "__main__":
+### Solution helper functions
+## Time series management
+def Suceptibles(sol): return sol[:,0]
+def Exposed(sol): return sol[:,1]
+def Quarantined(sol): return sol[:,2]
+def Asymptomatic(sol): return sol[:,3]
+def Infected(sol): return sol[:,4]
+def Hospitalized(sol): return sol[:,5]
+def Recovered(sol): return sol[:,6]
+def Deceased(sol): return sol[:,7]
+def TotalCases(sol): return Infected(sol) + Hospitalized(sol) + Recovered(sol) + Deceased(sol)
+def ICUcases(sol): return Hospitalized(sol) + Recovered(sol) + Deceased(sol)
+## Aggregation
+def CasesAggregation(sol_per_state, f=TotalCases): return np.sum( [f(sol) for sol in sol_per_state], axis=0   )
 
-    ## READING DATA ##
-    # ElLeo data: mexican states
-    mex_confirmed = pd.read_csv(DATA_URL_MEX+'covid19_mex_casos_totales.csv', )
-    mex_deaths = pd.read_csv(DATA_URL_MEX+'covid19_mex_muertes.csv', )
-    mex_recovered = pd.read_csv(DATA_URL_MEX+'covid19_mex_recuperados.csv', )
+run = True
+if run:
+    if __name__ == "__main__":
 
-    ## SETTING MODEL PARAMETERS ##
-    R_0 = 2.3 # ±0.3 # empirical basic reproductive ratio
-    # Arenas parameters
-    β = 0.06 # infectivity of the desease (per contact per day)
-    k_avg = 13.3 # average contacts per day. (11.8, 13.3, 6.6) in the report
-    μg = 1/3.2 # escape (from Infected) rate. (1/1, 1/3.2, 1/3.2) in the report
-    γg = 0.05 # fraction of cases requiring ICU. (0.002, 0.05, 0.36) in the report
-    # γg should be a weighted sum of %_young * 0.002 + %_mid * 0.05 + %_old * 0.36
-    ωg = 0.42 # fatality rate of ICU patients
-    ψg = 1/7  # death rate
-    χg = 1/10 # ICU discharge rate
+        ## READING DATA ##
+        # ElLeo data: mexican states
+        DATA_URL_MEX = 'https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/'
 
-    # SEIHRD params
-    κ = 1/5.2 # η^-1 + α^-1
-    γI = μg*(1 - γg)
-    μI = 0
-    α = μg*γg
-    μH = ωg*ψg
-    γH = (1 - ωg)*χg
-    λ = -k_avg * np.log( 1 - β ) # linearization of Π(t) of Arenas' report
-    # λ = R_0 * (γI + μI + α)  # using the R0
+        mex_confirmed = pd.read_csv(DATA_URL_MEX+'covid19_mex_casos_totales.csv', )
+        mex_deaths = pd.read_csv(DATA_URL_MEX+'covid19_mex_muertes.csv', )
+        mex_recovered = pd.read_csv(DATA_URL_MEX+'covid19_mex_recuperados.csv', )
 
-    params = (λ, κ, γI, μI, α, γH, μH)
+        ### PARAMETER ESTIMATION ###
+        # population distribution as of 2020ish
+        N_mex = 128_569_304 # https://www.worldometers.info/world-population/mexico-population/ (2020-03-34)
+        pop_dist_mex = np.array([55545770, 62567894, 9461864])/N_mex
 
-    ## INITIAL CONDITIONS SETUP ##
-    N_mex = 128_569_304 # https://www.worldometers.info/world-population/mexico-population/ (2020-03-34)
-    initial_date = '2020-03-12' # This is the date in which the trend becomes exponential
+        # Basic reproductive ratio
+        R_0 = 2.3
+        ## Arenas params
+        β = 0.06 # infectivity of the desease (per contact per day)
+        η = 1/2.34 # η^-1 + α^-1 = 1/5.2 # exposed latent rate
+        ωg = 0.42 # fatality rate of ICU patients
+        ψg = 1/7  # death rate
+        χg = 1/10 # ICU discharge rate
+        σ  = 3.7 # average household size in Mexico (different from Arenas)
+        μg = np.array([1/1,1/3.2,1/3.2]) # escape (from Infected) rate by age group
+        γg = np.array([0.002, 0.05, 0.36]) # fraction of cases requiring ICU by age group
+        kg = np.array([11.8, 13.3, 6.6]) # average contacts per day by age group
+        αg = np.array([1/5.06, 1/2.86, 1/2.86]) # asymptomatic infectious rate by age group
 
-    confirmed = national_timeseries(mex_confirmed).loc[initial_date,'Mexico_pais']
-    deaths = national_timeseries(mex_deaths).loc[initial_date,'Mexico_pais']
-    recovered = national_timeseries(mex_recovered).loc[initial_date,'Mexico_pais']
+        ## avering out the age groups with the pyramidal distribution of Mexico
+        μ_avg = np.dot(μg, pop_dist_mex)
+        γ_avg = np.dot(γg, pop_dist_mex)
+        k_avg = np.dot(kg, pop_dist_mex)
 
-    r = 6 # proportion of unobserved (exposed) cases
-    R0 = recovered / N_mex
-    D0 = deaths / N_mex
-    I0 = (confirmed/N_mex) - R0 - D0  # confirmed cases
-    E0 = r*I0 # cases without symptoms, which are not yet detected
-    H0 = 0 # no hospitalization data yet
-    S0 = (1 - E0 - I0 - R0 - D0 - H0)
+        ## SEAIHRD model params
+        β
+        k_avg
+        η
+        α = np.dot(αg, pop_dist_mex)
+        γI = μ_avg*(1 - γ_avg)
+        μI = 0
+        ν = μ_avg*γ_avg
+        μH = ωg*ψg
+        γH = (1 - ωg)*χg
 
-    x0 = [S0, E0, I0, H0, R0, D0]
+        ### INITIAL CONDITIONS PER STATE SETUP ###
+        # population per state in Mexico
+        population_per_state_mex = pd.read_csv('./data/poblaciones_estados.csv', index_col=0).sort_index()['population'].values
+        # list of states
+        states_mex = pd.read_csv('./data/poblaciones_estados.csv', index_col=0).sort_index().index.values
 
-    ### DYNAMICS ###
-    prediction_horizon = 5 # days
-    n_days = (datetime.datetime.today() - datetime.datetime.strptime(initial_date, '%Y-%m-%d')).days + prediction_horizon
+        # initial date for the model
+        initial_date = '2020-03-19' # This is the date in which the trend becomes exponential
 
-    sol = solve(SEIHRD_markov_step, x0, 0.0, n_days, *params)
+        # model projection setup
+        tc = 6 # containtment intervention date (days since initial_date)
+        projection_horizon = 8 # days
+        # number of days to run the model for
+        n_days = (datetime.datetime.today() - datetime.datetime.strptime(initial_date, '%Y-%m-%d')).days + projection_horizon
 
-    ### SAVING RESULTS ###
-    sol_to_csv('covid19_mex_proyecciones.csv', sol, initial_date, N_mex)
-    print('Done! Projections made for Mexico with initial condition at t0 = {} with data available until {}'.format(initial_date, mex_confirmed.Fecha.values[-1]))
+        ## Preallocation ##
+        # Vector of initial conditions. One set of initial conditions for each state
+        x0_vec = []
+        # Vectors of solutions. Each vector will contain the results for each state for a specific containtment scenario
+        projections_susana1 = []
+        projections_susana2 = []
+        projections_susana3 = []
+
+        print('Initial date: {}\n'.format(initial_date))
+        for (i,state) in enumerate(states_mex):
+
+            # population for state_i
+            N = population_per_state_mex[i]
+
+            # cases for state_i (from data)
+            confirmed = statal_timeseries(mex_confirmed).loc[initial_date, state]
+            deaths = statal_timeseries(mex_deaths).loc[initial_date, state]
+            recovered = statal_timeseries(mex_recovered).loc[initial_date, state]
+
+            print('{} has a population of {} people'.format(state, N))
+
+            ## initial conditions of state_i setup ##
+            r = 0.7 # r denotes the fraction of latent cases with respect to the confirmed cases. This is, E0 + A0 = r * I0
+            R0 = recovered / N              # fraction of recovered
+            D0 = deaths / N                 # fraction of deceased
+            I0 = ((confirmed/N) - R0 - D0)  # fraction of confirmed infected cases
+            E0 = 1*r/2 * I0                 # fraction of exposed non-infectious cases. Latent variable
+            A0 = 1*r/2 * I0                 # fraction of asymptomatic but infectious cases. Latent variable
+            H0 = 0                          # fraction of hospitalized cases. No data yet
+            CH0 = 0                         # fraction of self-isolated cases. 0 if no prevention is made by the government
+            S0 = (1 - E0 - A0 - I0 - R0 - D0 - H0) # fraction of suceptible cases
+
+            print('with', (I0 + R0 + D0 + H0)*N, 'total cases.' )
+
+            # inital conditions of state_i
+            x0 = np.array([S0, E0, CH0, A0, I0, H0, R0, D0])
+
+            # append state_i to the vector of initial conditions
+            x0_vec.append(x0)
+
+            ### MODEL SIMULATIONS FOR VARIOUS CONTAINTMENT SCENARIOS ###
+
+            ## Scenario 1: No action
+            κ0 = 0.0
+            params_1 = (β, k_avg, η, α, γI, μI, ν, γH, μH, κ0, σ, tc)
+
+            ## Scenario 2: Mild distancing
+            κ0 = 0.2
+            params_2 = (β, k_avg, η, α, γI, μI, ν, γH, μH, κ0, σ, tc)
+
+            ## Scenario 3: Strong distancing
+            κ0 = 0.5
+            params_3 = (β, k_avg, η, α, γI, μI, ν, γH, μH, κ0, σ, tc)
+
+            ### run models for state_i ###
+            print('Projecting for {}'.format( state ))
+            projection_state_i_scenario_1 = solve(SEIHRD_markov_step, x0_vec[i], 0.0, n_days, *params_1) * N
+            projection_state_i_scenario_2 = solve(SEIHRD_markov_step, x0_vec[i], 0.0, n_days, *params_2) * N
+            projection_state_i_scenario_3 = solve(SEIHRD_markov_step, x0_vec[i], 0.0, n_days, *params_3) * N
+
+            # Join model simulation for state_i in the vector of solutions
+            projections_susana1.append( projection_state_i_scenario_1 )
+            projections_susana2.append( projection_state_i_scenario_2 )
+            projections_susana3.append( projection_state_i_scenario_3 )
+
+        ### SAVING RESULTS ###
+        PLOT_PATH = './media/covid19_mex_proyecciones.png'
+        CSV_PATH  = './results/'
+
+        ## Plot the projections of the model at a national level showing each scenario
+        plt.figure( figsize=(10,8) )
+        mae = plot_cases(CasesAggregation(projections_susana1), mex_confirmed, 'México', initial_date, ls='-', label_proj='$0 \%$ Susana')
+        plot_cases(CasesAggregation(projections_susana2), mex_confirmed, 'México', initial_date, ls='--', label_proj='$20 \%$ Susana')
+        plot_cases(CasesAggregation(projections_susana3), mex_confirmed, 'México', initial_date, ls='-.', label_proj='$50 \%$ Susana', label_data='Casos Totales')
+        plt.axvline([tc], c='black', alpha=0.8)
+        plt.title( 'Casos totales de COVID-19 en {}. (MAE = {}) '.format('México', round(mae,1) ) , size=16)
+        plt.ylabel('Número de infectados', size=15);
+        # plt.yscale('log')
+        plt.tight_layout()
+        plt.savefig(PLOT_PATH)
+
+        sol_to_csv(CSV_PATH+'covid19_mex_proyeccion_susana_00.csv', projections_susana1, initial_date)
+        sol_to_csv(CSV_PATH+'covid19_mex_proyeccion_susana_20.csv', projections_susana2, initial_date)
+        sol_to_csv(CSV_PATH+'covid19_mex_proyeccion_susana_50.csv', projections_susana3, initial_date)
