1752a9a6f853b327fb5a420e56fe33d5da5ba2b1
Blas Kolic <blas.kolic@ouce.ox.ac.uk>
2020-04-01 11:33:07 +0100

    Updated repo structure and modified model script.

--- src/run_SEAIHRD_model.py
+++ src/run_SEAIHRD_model.py
@@ -78,7 +78,7 @@ def ICUcases(sol): return Hospitalized(sol) + Deceased(sol)
 def CasesAggregation(sol_per_state, f=TotalCases): return np.sum( [f(sol) for sol in sol_per_state], axis=0   )
 
 # takes a set of solutions, aggregates them and saves them in a csv
-def scenario_to_csv(filename, sol, initial_date):
+def scenario_to_csv(filename, sol, initial_date, print_=False):
     '''
     Saves a return a single model output for a given scenario `sol` in a csv
     that contains the dynamics of each compartiment in each column.
@@ -102,11 +102,13 @@ def scenario_to_csv(filename, sol, initial_date):
     CSV['Fecha'] = t_range
     CSV.set_index('Fecha', inplace=True)
 
-    print('Saved projections in {}'.format(filename))
+    if print_:
+        print('Saved projections in {}'.format(filename))
+
     CSV.to_csv(filename)
     return CSV
 
-def scenarios_to_csv(filename, dataset, scenarios, initial_date, f=TotalCases, R0_index=''):
+def total_cases_scenarios_to_csv(filename, dataset, scenarios, initial_date, f=TotalCases, R0_index=''):
     '''
     Saves and returns a csv file where the first column 'Totales' presents the available COVID-19 data in
     Mexico to date. The remaining columns are the fits+projections obtained with the model under different
@@ -290,7 +292,7 @@ if __name__ == "__main__":
     # Basic reproductive ratio
     # R_0s : 2.3, 95%-CI : (1.4, 3.9), according to Qun Li et al. 'Early Transmission Dynamics in Wuhan, China, ...'
     # ToDo: Do the R_0 thing automatically
-    R0s = {'': 2.3, '_min': 1.4, '_max': 3.9}
+    R0s = {'_min': 1.4, '_max': 3.9, '': 2.3}
     for R0ix in R0s:
         R_0 = R0s[R0ix]
         print('Doing R_0 = {}'.format(R_0))
@@ -325,16 +327,16 @@ if __name__ == "__main__":
 
         ### INITIAL CONDITIONS PER STATE SETUP ###
         # population per state in Mexico
-        population_per_state_mex = pd.read_csv('./data/poblaciones_estados.csv', index_col=0).sort_index()['population'].values
+        population_per_state_mex = pd.read_csv('../data/poblaciones_estados.csv', index_col=0).sort_index()['population'].values
         # list of states
-        states_mex = pd.read_csv('./data/poblaciones_estados.csv', index_col=0).sort_index().index.values
+        states_mex = pd.read_csv('../data/poblaciones_estados.csv', index_col=0).sort_index().index.values
 
         # initial date for the model
         initial_date = '2020-03-19' # -19 This is the date in which the trend becomes exponential
 
         # model projection setup
         tc = 6 # containtment intervention date (days since initial_date)
-        projection_horizon = 60 # 6 days
+        projection_horizon = 5 # 6 days
         # number of days to run the model for
         n_days = (datetime.datetime.today() - datetime.datetime.strptime(initial_date, '%Y-%m-%d')).days + projection_horizon
 
@@ -354,6 +356,7 @@ if __name__ == "__main__":
         projections_susana2 = []
         projections_susana3 = []
 
+        print_ = False
         print('Initial date: {}\n'.format(initial_date))
         for (i,state) in enumerate(states_mex):
 
@@ -364,8 +367,6 @@ if __name__ == "__main__":
             confirmed = statal_timeseries(mex_confirmed).loc[initial_date, state]
             deaths = statal_timeseries(mex_deaths).loc[initial_date, state]
             recovered = statal_timeseries(mex_recovered).loc[initial_date, state]
-
-            print('{} has a population of {} people'.format(state, N))
 
             ## initial conditions of state_i setup ##
             # r = 1 # 0.65 r denotes the fraction of latent cases with respect to the confirmed cases. This is, E0 + A0 = r * I0
@@ -378,8 +379,6 @@ if __name__ == "__main__":
             H0 = 0                          # fraction of hospitalized cases. No data yet
             CH0 = 0                         # fraction of self-isolated cases. 0 if no prevention is made by the government
             S0 = (1 - E0 - A0 - I0 - R0 - D0 - H0) # fraction of suceptible cases
-
-            print('with', (I0 + R0 + D0 + H0)*N, 'total cases.' )
 
             # inital conditions of state_i
             x0 = np.array([S0, E0, CH0, A0, I0, H0, R0, D0])
@@ -403,10 +402,14 @@ if __name__ == "__main__":
             projection_state_i_scenario_2 = solve(SEAIHRD_markov_step, x0, 0.0, n_days, *params_2) * N
             projection_state_i_scenario_3 = solve(SEAIHRD_markov_step, x0, 0.0, n_days, *params_3) * N
 
-            data = statal_timeseries(mex_confirmed).loc[initial_date:,state]
-            projection = TotalCases(projection_state_i_scenario_1)
-            tf_data = data.index.values[-1]
-            print('Projection for {} at {}: {} cases vs {} oficial cases. MAE: ({})'.format( state, tf_data, np.round(projection[-projection_horizon-1]), data.values[-1], round(MAE(data, projection), 1) ))
+            ## print summary of results per state if necessary.
+            if print_:
+                data = statal_timeseries(mex_confirmed).loc[initial_date:,state]
+                projection = TotalCases(projection_state_i_scenario_1)
+                tf_data = data.index.values[-1]
+                print('Projection for {} at {}: {} cases vs {} oficial cases. MAE: ({})'.format( state, tf_data, np.round(projection[-projection_horizon-1]), data.values[-1], round(MAE(data, projection), 1) ))
+                print('{} has a population of {} people'.format(state, N))
+                print('with', (I0 + R0 + D0 + H0)*N, 'total cases.' )
 
             # Join model simulation for state_i in the vector of solutions
             projections_susana1.append( projection_state_i_scenario_1 )
@@ -414,38 +417,61 @@ if __name__ == "__main__":
             projections_susana3.append( projection_state_i_scenario_3 )
 
         ### SAVING RESULTS ###
-        PLOT_PATH = './media/'
-        CSV_PATH  = './results/'
-
-        ## Plot the projections of the model at a national level showing each scenario
-        plt.figure( figsize=(10,8) )
-        mae = plot_cases(CasesAggregation(projections_susana1), mex_confirmed, 'México', initial_date, ls='-', label_proj='$0 \%$ Susana')
-        plot_cases(CasesAggregation(projections_susana2), mex_confirmed, 'México', initial_date, ls='--', label_proj='$20 \%$ Susana')
-        plot_cases(CasesAggregation(projections_susana3), mex_confirmed, 'México', initial_date, ls='-.', label_proj='$50 \%$ Susana', label_data='Casos Totales')
-        plt.axvline([tc], c='black', alpha=0.8)
-        plt.title( 'Casos totales de COVID-19 en {}. (MAE = {}) '.format('México', round(mae,1) ) , size=16)
-        plt.ylabel('Número de infectados', size=15);
-        # plt.yscale('log')
-        plt.tight_layout()
+        PLOT_PATH = '../media/'
+        CSV_PATH  = '../results/'
 
         # here I construct, for every scenario and every value of R0, a CSV with all the corresponding projections
         scenarios = [projections_susana1, projections_susana2, projections_susana3]
-        df_ = scenarios_to_csv(None, mex_confirmed, scenarios, initial_date, f=TotalCases, R0_index=R0ix)
+        # first argument = None makes the function to not save the df
+        df_ = total_cases_scenarios_to_csv(None, mex_confirmed, scenarios, initial_date, f=TotalCases, R0_index=R0ix)
         CSV = CSV.join(df_, how='outer')
 
         save_ = True
+        # Save individual runs with all the compartiments
         if save_:
             today_date = datetime.datetime.today().strftime('%d-%m-%y')
-            plt.savefig( PLOT_PATH+'covid19_mex_proyecciones_{}_R0_{}.png'.format( today_date, str(R_0).replace('.','p')) )
-            # sol_to_csv(CSV_PATH+'covid19_mex_proyeccion_susana_00_{}.csv'.format( today_date ), projections_susana1, initial_date)
-            # sol_to_csv(CSV_PATH+'covid19_mex_proyeccion_susana_20_{}.csv'.format( today_date ), projections_susana2, initial_date)
-            # sol_to_csv(CSV_PATH+'covid19_mex_proyeccion_susana_50_{}.csv'.format( today_date ), projections_susana3, initial_date)
-            print( 'covid19_mex_proyecciones_{}.csv'.format( today_date ) )
+            scenario_to_csv(CSV_PATH+'other/susana_00_{}_R0_{}.csv'.format( today_date, str(R_0).replace('.','p')), projections_susana1, initial_date)
+            scenario_to_csv(CSV_PATH+'other/susana_20_{}_R0_{}.csv'.format( today_date, str(R_0).replace('.','p')), projections_susana2, initial_date)
+            scenario_to_csv(CSV_PATH+'other/susana_50_{}_R0_{}.csv'.format( today_date, str(R_0).replace('.','p')), projections_susana3, initial_date)
 
+    ## Creating dataframe with scenarios and error bars ##
+    Data = national_timeseries(mex_confirmed)
+    Data['México'] = Data['México'].astype(int)
+    CSV = Data.join(CSV, how='outer')
+
+    save_ = True
     if save_:
-        Data = national_timeseries(mex_confirmed)
-        Data['México'] = Data['México'].astype(int)
-        CSV = Data.join(CSV, how='outer')
 
-        # str(R_0).replace('.','p')
-        CSV.to_csv( CSV_PATH+'covid19_mex_proyeccioneseee_{}.csv'.format( today_date) )
+        ## Saving results to csv ##
+        CSV.to_csv( CSV_PATH+'covid19_mex_proyecciones_{}.csv'.format( today_date) )
+
+        ## Plotting ##
+        plt.figure( figsize=(10,8) )
+        # containtment scenario 1
+        plt.plot(CSV['Susana_50'], lw=3, color='yellow', label='$50 \%$ Susana')
+        plt.fill_between(CSV.index.values, CSV['Susana_50_min'].values, CSV['Susana_50_max'].values,
+                         alpha=0.2, color='yellow');
+        # containtment scenario 2
+        plt.plot(CSV['Susana_20'], lw=3, color='red', label='$20 \%$ Susana')
+        plt.fill_between(CSV.index.values, CSV['Susana_20_min'].values, CSV['Susana_20_max'].values,
+        alpha=0.2, color='red');
+        # no containtment scenario
+        plt.plot(CSV['Susana_00'], lw=3, color='blue', label='$0 \%$ Susana')
+        plt.fill_between(CSV.index.values, CSV['Susana_00_min'].values, CSV['Susana_00_max'].values,
+                         alpha=0.2, color='blue');
+        ## Plot total cases data
+        plt.plot(national_timeseries(mex_confirmed)['2020-03-14':], marker='o', ms=9, lw=0, color='black', alpha=0.7, label='datos')
+        plt.axvline([tc], c='black', alpha=0.8)
+        plt.title( 'Casos totales de COVID-19 en {}.'.format('México') , size=16)
+        plt.ylabel('Número de infectados', size=15);
+        plt.legend(loc='upper left')
+        plt.ylim(-50,  np.minimum( CSV['Susana_00'].max() * 1.25, CSV['Susana_00_max'].max()) )
+        # plt.yscale('log')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        ## Saving results plot ##
+          # NOTE: The following warning appear but it doesn't affect the script:
+          # 'Source ID 8 was not found when attempting to remove it GLib.source_remove(self._idle_draw_id)''
+        plt.savefig( PLOT_PATH+'covid19_mex_proyecciones.png')
+
+    print('DONE!')
